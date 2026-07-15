557d124cb2b2eb6e52532eda87f448f123c7b4de
ruzenathegoat <naufalrahaman013@gmail.com>
2026-07-16 00:00:43 +0700

    Add global rate limit (10 reqs / hour)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->web(append: [
+            'throttle:10,60',
+        ]);
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
