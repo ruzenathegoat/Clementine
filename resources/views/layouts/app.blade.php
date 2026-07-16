c0bce3dfe99561013712fc70959c0c0a10d52597
ruzenathegoat <naufalrahaman013@gmail.com>
2026-07-17 01:14:46 +0700

    fix: add missing csrf token for pusher auth and fetch requests on customer side

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'CLEMENTINE')</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet">
